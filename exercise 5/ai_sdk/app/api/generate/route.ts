4e61dbe65e5a8eb2315f2aa258585f89cf4d078e
marcossbc <marcossbc880@gmail.com>
2026-07-14 17:09:06 +0300

    aded besicpage

---
+++ exercise 5/ai_sdk/app/api/generate/route.ts
@@ -0,0 +1,14 @@
+import { openai } from '@ai-sdk/openai';
+
+import {generateText} from "ai"
+
+export async function POST(req: Request) {
+  const { prompt } = await req.json();
+
+  const { text } = await generateText({
+    model: openai('gpt-4o'),
+    prompt,
+  });
+
+  return Response.json({ text });
+}
